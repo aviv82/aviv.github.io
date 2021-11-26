22fb0557100a15119b9fa0fcf6b00407d24e2fee
Aviv <aviv.dror@gmail.com>
2021-11-26 06:00:26 +0100

    add social media links

--- js/index.js
+++ js/index.js
@@ -111,7 +111,7 @@ function dot() {
   for (var i = 0; i < dot.length; i++) {
     var windowheight = window.innerHeight;
     var dottop = dot[i].getBoundingClientRect().top;
-    var dotpoint = 200;
+    var dotpoint = 60;
 
     if (dottop < windowheight - dotpoint) {
       dot[i].classList.add("dotted");
@@ -128,7 +128,7 @@ function boxo() {
   for (var i = 0; i < boxo.length; i++) {
     var windowheight = window.innerHeight;
     var boxotop = boxo[i].getBoundingClientRect().top;
-    var boxopoint = 200;
+    var boxopoint = 80;
 
     if (boxotop < windowheight - boxopoint) {
       boxo[i].classList.add("boxero");
@@ -145,7 +145,7 @@ function boxi() {
   for (var i = 0; i < boxi.length; i++) {
     var windowheight = window.innerHeight;
     var boxitop = boxi[i].getBoundingClientRect().top;
-    var boxipoint = 220;
+    var boxipoint = 80;
 
     if (boxitop < windowheight - boxipoint) {
       boxi[i].classList.add("boxeri");
@@ -179,12 +179,29 @@ function but() {
   for (var i = 0; i < but.length; i++) {
     var windowheight = window.innerHeight;
     var boxstop = but[i].getBoundingClientRect().top;
-    var boxspoint = 100;
+    var boxspoint = 200;
 
     if (boxstop < windowheight - boxspoint) {
       but[i].classList.add("button");
     } else {
       but[i].classList.remove("button");
+    }
+  }
+}
+window.addEventListener("scroll", buts);
+
+function buts() {
+  var buts = document.querySelectorAll(".buts");
+
+  for (var i = 0; i < buts.length; i++) {
+    var windowheight = window.innerHeight;
+    var boxstop = buts[i].getBoundingClientRect().top;
+    var boxspoint = 60;
+
+    if (boxstop < windowheight - boxspoint) {
+      buts[i].classList.add("buttons");
+    } else {
+      buts[i].classList.remove("buttons");
     }
   }
 }
--- planning/contributors.md
+++ planning/contributors.md
@@ -1,11 +1,12 @@
 # contributors
 
->dear contributors, thank you for checking out my project! 
->i welcome and appreciate your help. 
+> dear contributors, thank you for checking out my project! i welcome and
+> appreciate your help.
 
 ## before contrributing
-> please consult the 
-> [code of conduct](https://github.com/aviv82/aviv.home/blob/8c6c368fc440d79a6d56eaa0327502554f49e10f/CODE-OF-CONDUCT.md) 
+
+> please consult the
+> [code of conduct](https://github.com/aviv82/aviv.home/blob/8c6c368fc440d79a6d56eaa0327502554f49e10f/CODE-OF-CONDUCT.md)
 > and [creative license](https://github.com/aviv82/aviv.home/blob/main/LICENSE)
 
-### Thank you for your support! 
+### Thank you for your support
